876b15944c4853570742b371266ae855a76de09d
Codepressd <creederone@gmail.com>
2017-01-10 04:56:44 -0800

    solved project css responsive issue

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-	
+/* Open when someone clicks on the span element */
+function openNav(project) {
+    document.getElementById(project).style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav(project) {
+    document.getElementById(project).style.width = "0%";
+}	
